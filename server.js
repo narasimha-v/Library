0ad07363e21facbfb282233d6ce5de77c91f3023
Narasimha-v <narasimha.v@mercer.com>
2020-01-02 20:04:45 +0530

    commit

---
+++ server.js
@@ -0,0 +1,28 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+const expresslayout = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const indexRouter = require('./routes/index');
+
+app.set('view engine','ejs');
+app.set('views',__dirname + '/views');
+app.set('layout','layouts/layout');
+app.use(expresslayout);
+app.use(express.static('public'));
+
+app.use('/',indexRouter)
+
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true});
+mongoose.connection.on('error',error=>console.error(error));
+mongoose.connection.once('open',()=>console.log('Connected to Mongo DB'));
+
+app.listen(process.env.PORT,(err)=>{
+    if(!err){
+        console.log('Server Started');       
+    }
+})
